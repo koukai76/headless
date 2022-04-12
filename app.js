bb6e35cf27ee40abda5fc70d76a82b6af888b822
koukai76 <61773788+koukai76@users.noreply.github.com>
2022-04-12 14:03:20 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const URL = 'https://www.serversus.work/';
+  // const URL = 'https://www.serversus.work/';
+  const URL = 'https://www.yahoo.co.jp/';
 
   const browser = await puppeteer.launch({});
   const page = await browser.newPage();
@@ -9,11 +10,17 @@ const puppeteer = require('puppeteer');
 
   // const html = await page.content();
 
-  const ret = await page.evaluate(() => {
-    return document.querySelectorAll('.article-list li')[0].textContent;
-  });
+  // const ret = await page.evaluate(() => {
+  //   return document.querySelectorAll('.article-list li')[0].textContent;
+  // });
+  // console.log(ret);
 
-  console.log(ret);
+  const ret = await page.evaluate(() => {
+    return Array.from(document.querySelectorAll('section ul article h1')).map(
+      m => m.textContent
+    );
+  });
+  ret.map(m => console.log(m));
 
   await browser.close();
 })();
