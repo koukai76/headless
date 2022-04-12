4ba1fecaa1dcd80b8bfa910bff94f79f863ba8aa
koukai76 <61773788+koukai76@users.noreply.github.com>
2022-04-13 01:31:43 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const puppeteer = require('puppeteer');
 const mysql = require('mysql2');
-require('dotenv').config();
+// require('dotenv').config();
 
 const connection = mysql.createConnection({
-  host: process.env.HOST100,
-  database: process.env.DATABASE100,
-  user: process.env.USER100,
-  password: process.env.PASSWORD100,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
   ssl: {
     rejectUnauthorized: true,
   },
@@ -25,38 +25,24 @@ const query = (sql, params) => {
   });
 };
 
-// const query = (sql, params) => {
-//   return new Promise((resolve, reject) => {
-//     connection.query(sql, params, (err, results, fields) => {
-//       if (err) {
-//         reject(err);
-//         return;
-//       }
-
-//       resolve({ results: results, fields: fields });
-//     });
-//   });
-// };
-
 (async () => {
-  // console.log(await connection.getMaxListeners());
   const ret = await query('SELECT * FROM users');
   console.log(ret.results);
   await connection.end()
   
-  // const URL = 'https://www.serversus.work/';
+  const URL = 'https://www.serversus.work/';
   // const URL = 'https://www.yahoo.co.jp/';
 
-  // const browser = await puppeteer.launch({});
-  // const page = await browser.newPage();
-  // await page.goto(URL, { waitUntil: 'networkidle0' });
+  const browser = await puppeteer.launch({});
+  const page = await browser.newPage();
+  await page.goto(URL, { waitUntil: 'networkidle0' });
 
   // const html = await page.content();
 
-  // const ret = await page.evaluate(() => {
-  //   return document.querySelectorAll('.article-list li')[0].textContent;
-  // });
-  // console.log(ret);
+  const ret = await page.evaluate(() => {
+    return document.querySelectorAll('.article-list li')[0].textContent;
+  });
+  console.log(ret);
 
   // const ret = await page.evaluate(() => {
   //   return Array.from(document.querySelectorAll('section ul article a')).map(
@@ -74,8 +60,5 @@ const query = (sql, params) => {
   //   console.log(m.href + '\n');
   // });
 
-  // await browser.close();
-
-  //   const ret2 = await query('SELECT * FROM users');
-  //   console.log(ret2.results);
+  await browser.close();
 })();
