b8a79db5af8db1ca95e207439eb63f8fa59cf6f4
koukai76 <61773788+koukai76@users.noreply.github.com>
2022-04-13 01:14:28 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,39 +1,37 @@
 const puppeteer = require('puppeteer');
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 // require('dotenv').config();
 
-const connection = mysql.createConnection({
-  host: process.env.HOST,
-  database: process.env.DATABASE,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  ssl: {
-    rejectUnauthorized: true,
-  },
-});
+// const connection = mysql.createConnection({
+//   host: process.env.HOST,
+//   database: process.env.DATABASE,
+//   user: process.env.USER,
+//   password: process.env.PASSWORD,
+//   ssl: {
+//     rejectUnauthorized: true,
+//   },
+// });
 
-const query = (sql, params) => {
-  return new Promise((resolve, reject) => {
-    connection.query(sql, params, (err, results, fields) => {
-      if (err) {
-        reject(err);
-        return;
-      }
+// const query = (sql, params) => {
+//   return new Promise((resolve, reject) => {
+//     connection.query(sql, params, (err, results, fields) => {
+//       if (err) {
+//         reject(err);
+//         return;
+//       }
 
-      resolve({ results: results, fields: fields });
-    });
-  });
-};
+//       resolve({ results: results, fields: fields });
+//     });
+//   });
+// };
 
 (async () => {
-  console.log(process.env.DATABASE + "bb");
-  
   // const URL = 'https://www.serversus.work/';
   const URL = 'https://www.yahoo.co.jp/';
 
-  const browser = await puppeteer.launch({});
-  const page = await browser.newPage();
-  await page.goto(URL, { waitUntil: 'networkidle0' });
+  // const browser = await puppeteer.launch({});
+  // const page = await browser.newPage();
+  // await page.goto(URL, { waitUntil: 'networkidle0' });
 
   // const html = await page.content();
 
@@ -42,24 +40,24 @@ const query = (sql, params) => {
   // });
   // console.log(ret);
 
-  const ret = await page.evaluate(() => {
-    return Array.from(document.querySelectorAll('section ul article a')).map(
-      m => {
-        return {
-          title: m.textContent,
-          href: m.href
-        }
-      }
-    );
-  });
-  
-  ret.map(m => {
-    console.log(m.title);
-    console.log(m.href + "\n")
-  });
+  // const ret = await page.evaluate(() => {
+  //   return Array.from(document.querySelectorAll('section ul article a')).map(
+  //     m => {
+  //       return {
+  //         title: m.textContent,
+  //         href: m.href,
+  //       };
+  //     }
+  //   );
+  // });
 
-  await browser.close();
-  
-//   const ret2 = await query('SELECT * FROM users');
-//   console.log(ret2.results);
+  // ret.map(m => {
+  //   console.log(m.title);
+  //   console.log(m.href + '\n');
+  // });
+
+  // await browser.close();
+
+  //   const ret2 = await query('SELECT * FROM users');
+  //   console.log(ret2.results);
 })();
